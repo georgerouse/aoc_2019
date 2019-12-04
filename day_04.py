fa1dc34140ef1f6c74323ced9a150bebb536bcbc
George Rouse <george.rouse@gmail.com>
2019-12-04 21:00:49 +0000

    Update day_04.py

--- day_04.py
+++ day_04.py
@@ -1,37 +1,41 @@
-if __name__ == '__main__':    
+from collections import Counter
+
+if __name__ == '__main__':
+    # Generate a list of all possible passwords
     num_list = [str(i) for i in range(347312, 805915+1)]
-    password_count = 0
-    
+    password_list = []
+
+    # Part 1:
     for num in num_list:
-        # Check two adjacent digits
         previous_digit = None
         has_two_adjacent_digits = False
         incrementing = True
-        
+
         for digit in num:
-            print(previous_digit, digit)
             if previous_digit is None:
                 previous_digit = digit
             else:
+                # Check two adjacent digits
                 if previous_digit == digit:
                     has_two_adjacent_digits = True
+
+                # Digits never decrease
                 if int(previous_digit) > int(digit):
                     incrementing = False
                     break
+
                 previous_digit = digit
-                    
-            if has_two_adjacent_digits and incrementing:
-                password_count += 1
-                print(num)
-                import pdb;pdb.set_trace()
-    print(password_count)
-           
 
+        if has_two_adjacent_digits and incrementing:
+            password_list.append(num)
 
-# Two adjacent digits are the same (like 22 in 122345).
-# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-# Other than the range rule, the following are true:
+    print("Number of different passwords:", len(password_list))
 
-# 111111 meets these criteria (double 11, never decreases).
-# 223450 does not meet these criteria (decreasing pair of digits 50).
-# 123789 does not meet these criteria (no double).
+    # Part 2:
+    filtered_password_list = []
+    for num in password_list:
+        counter = Counter(num)
+        if 2 in counter.values():
+            filtered_password_list.append(num)
+
+    print("Number of different passwords:", len(filtered_password_list))
